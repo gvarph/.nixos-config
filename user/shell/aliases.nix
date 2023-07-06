22790667e480f8c440a37cd5048ce0577aeda10e
Filip Krul <gvarph006@gmail.com>
2023-07-06 15:06:31 +0200

    more aliases

--- user/shell/aliases.nix
+++ user/shell/aliases.nix
@@ -8,6 +8,9 @@
     gc-check = "nix-store --gc --print-roots | egrep -v \"^(/nix/var|/run/\w+-system|\{memory|/proc)\"";
     no-rebuild-fast = "sudo nixos-rebuild switch --fast";
 
+    nix-fish = "nix-shell --run fish -p fish ";
+
+
     #navigation
     "cd." = "cd ..";
     "cd.." = "cd ../..";
