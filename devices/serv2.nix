a0dca08fe59c957e7a1ba3b08c4f978da69e53e1
Filip Krul <gvarph006@gmail.com>
2023-07-31 15:35:58 +0200

    added config for 2nd erver

---
+++ devices/serv2.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+
+{
+
+  imports = [
+    ../common.nix
+    ../system/features/docker.nix
+    ../system/filesystem/nas.nix
+  ];
+
+
+  networking.hostName = "serv2";
+  networking.networkmanager.enable = true;
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+}
