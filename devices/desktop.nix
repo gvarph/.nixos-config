38d9a72f5edecba3955ba4dfb439f4859442579d
Filip Krul <gvarph006@gmail.com>
2023-07-31 22:52:48 +0200

    added stuff for desktop

---
+++ devices/desktop.nix
@@ -0,0 +1,31 @@
+{ config,lib,  pkgs, ... }:
+
+
+{
+
+  imports = [
+    ../common.nix
+    ../system/features/docker.nix
+   # TODO:  ../system/filesystem/nas/mount.nix
+   ../hyprland/enable.nix  
+  ];
+  
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  }; 
+
+  networking.hostName = "deskt";
+  networking.networkmanager.enable = true;
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+}
