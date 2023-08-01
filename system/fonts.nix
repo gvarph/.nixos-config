2ccf2f3f95c416fd35328127a19a7e9b754b561f
Filip Krul <gvarph006@gmail.com>
2023-08-01 23:45:47 +0200

    fixed deprecated thingie

--- system/fonts.nix
+++ system/fonts.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
- fonts.fonts = with pkgs; [
-  (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-];
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
 }
