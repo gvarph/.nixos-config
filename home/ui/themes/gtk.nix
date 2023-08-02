1dd7d14aade6567e78824b2449888b368200bba0
Filip Krul <gvarph006@gmail.com>
2023-08-02 03:21:00 +0200

    found what was causing the error

--- home/ui/themes/gtk.nix
+++ home/ui/themes/gtk.nix
@@ -10,7 +10,7 @@
   #FIXME:
   gtk = {
     enable = true;
-    ##font.name = "${custom.font} ${custom.fontsize}";
+    #font.name = "${custom.font} ${custom.fontsize}";
     #iconTheme = {
     #  name = "Papirus-Dark";
     #  package = pkgs.catppuccin-papirus-folders;
@@ -19,19 +19,19 @@
     #  name = "${custom.cursor}";
     #  package = pkgs.numix-cursor-theme;
     #};
-    #    theme = {
-    #      name = "Catppuccin-Mocha-Standard-Blue-dark";
-    #      package = pkgs.catppuccin-gtk.override { variant = "mocha"; };
-    #    };
-    #    gtk3.extraConfig = {
-    #      Settings = ''
-    #        gtk-application-prefer-dark-theme=1
-    #      '';
-    #    };
-    #    gtk4.extraConfig = {
-    #      Settings = ''
-    #        gtk-application-prefer-dark-theme=1
-    #      '';
-    #    };
+    #theme = {
+    #  name = "Catppuccin-Mocha-Standard-Blue-dark";
+    #  package = pkgs.catppuccin-gtk.override { variant = "mocha"; };
+    #};
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
   };
 }
