4356256f6660844936562c9fa2af37ce25038ef4
Filip Krul <gvarph006@gmail.com>
2023-09-29 20:24:14 +0200

    updated serv2

--- devices/serv2.nix
+++ devices/serv2.nix
@@ -1,10 +1,14 @@
 { config, pkgs, ... }:
 
-
+let
+  username = "gvarph";
+  #gpuId = "10de:1f02";
+  #soundId = "10de:10f9";
+in
 {
 
   imports = [
-    ../common.nix
+        (import ../default.nix { inherit config pkgs username; })
     ../linux/features/docker.nix
     ../linux/filesystem/nas/mount.nix
   ];
