07c570e7f9a08a33f46182f916452d0c6f00d779
Filip Krul <gvarph006@gmail.com>
2023-10-12 13:23:49 +0200

    Updated serv1 config to add VPN to office

--- linux/vscode-server.nix
+++ linux/vscode-server.nix
@@ -1,23 +1,17 @@
 { pkgs, ... }:
 
-#let
-#  pkgs_with_old_code = import
-#    (builtins.fetchTarball {
-#      name = "nixos-unstable-2023-08-03";
-#      url = "https://github.com/nixos/nixpkgs/archive/525595ebff10594d3d6071ca9953ba15f7e8b19c.tar.gz";
-#      sha256 = "09chsdrlb3z5rsccxcw9h4a1vl7bm9jgxpg8v78ni5rgcm9api7r";
-#    })
-#    { config = { allowUnfree = true; }; };
-#in
+let
+  stable = import <nixos-stable> { config = { allowUnfree = true; }; };
+in
 {
 
-  # environment.systemPackages = [ pkgs_with_old_code.vscode ];
-  environment.systemPackages = [ pkgs.vscode ];
 
+            
+  programs.nix-ld.enable = true;
   imports = [
 	(fetchTarball "https://github.com/nix-community/nixos-vscode-server/tarball/master")
   ];
- # services.vscode-server.enable = true;  
+  services.vscode-server.enable = true;  
  
 }
 
