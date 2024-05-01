ffb36f41224bcceab4e6b5eca5f20370a4d3983c
Filip Krul <gvarph006@gmail.com>
2024-05-01 17:51:09 +0200

    add gitnignre

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hardware-configuration.nix.
+        ./devices/serv1.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
