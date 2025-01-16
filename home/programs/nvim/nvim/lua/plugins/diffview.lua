f9d2c291db3b4b9babbd155a7d188faca768ce30
Filip Krul <gvarph006@gmail.com>
2025-01-16 08:43:18 +0100

    Add diffview keybind for nvim

--- home/programs/nvim/nvim/lua/plugins/diffview.lua
+++ home/programs/nvim/nvim/lua/plugins/diffview.lua
@@ -2,5 +2,8 @@ return {
 	"sindrets/diffview.nvim",
 	config = function()
 		require("diffview").setup({})
+
+		-- Set <leader>dv to open Diffview
+		vim.api.nvim_set_keymap("n", "<leader>dv", ":DiffviewOpen<CR>", { noremap = true, silent = true })
 	end,
 }
