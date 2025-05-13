37d456b3b6630b6b8f2b01d82d44c7f77e5e16f8
Filip Krul <gvarph006@gmail.com>
2025-05-13 21:51:53 +0200

    change up nvim keybinds

--- home/programs/nvim/nvim/lua/plugins/treesitter.lua
+++ home/programs/nvim/nvim/lua/plugins/treesitter.lua
@@ -15,10 +15,10 @@ return {
 		incremental_selection = {
 			enable = true,
 			keymaps = {
-				init_selection = "<CR>",
-				node_incremental = "<CR>",
-				scope_incremental = "<S-CR>",
-				node_decremental = "<BS>",
+				init_selection = "gnn", -- set to `false` to disable one of the mappings
+				node_incremental = "grn",
+				scope_incremental = "grc",
+				node_decremental = "grm",
 			},
 		},
 	},
