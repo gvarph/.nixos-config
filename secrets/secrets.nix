cb19bfe2f0ad463e4cf2058eb08df0357e8c1b16
Filip Krul <gvarph006@gmail.com>
2025-05-23 22:12:19 +0200

    Rekey agenix with serv2 key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,6 @@
 let
   serv1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHSGf6aVu23GcNSbF82+NzrO0MfknMt31so4XsHFd0vn gvarph@serv1";
+  serv2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDE2TzrpOPaQ3Htd51zqES06PePo1E8fb9bemh9iQOJS gvarph@serv2";
 in {
-  "nas_auth.age".publicKeys = [serv1];
+  "nas_auth.age".publicKeys = [serv1 serv2];
 }
