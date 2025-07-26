306148466d20f82cac5084d3df64263e94c65999
Filip Krul <gvarph006@gmail.com>
2025-07-26 13:42:32 +0200

    Mount personal nas dir

--- linux/filesystem/nas/mount.nix
+++ linux/filesystem/nas/mount.nix
@@ -17,4 +17,18 @@
     ];
     mountPoint = "/remote/nas/media";
   };
+  fileSystems."nas-personal" = {
+    device = "//192.168.31.7/personal";
+    fsType = "cifs";
+    options = [
+      "credentials=${config.age.secrets.nas_auth.path}"
+      "uid=1000"
+      "x-systemd.automount" # auto-mount on access
+      "noauto" # don't mount on boot
+      "x-systemd.idle-timeout=60" # unmount after 60 seconds of inactivity
+      "x-systemd.device-timeout=5s" #  device timeout for the automount unit
+      "x-systemd.mount-timeout=5s" #  mount timeout for the automount unit
+    ];
+    mountPoint = "/remote/nas/personal";
+  };
 }
