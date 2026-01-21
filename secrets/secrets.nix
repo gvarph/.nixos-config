ad6a430d7e2d610d5ff33a3a440ea0e22a6895d8
Filip Krul <gvarph006@gmail.com>
2026-01-21 17:44:29 +0100

    Add desktop ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   serv1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHSGf6aVu23GcNSbF82+NzrO0MfknMt31so4XsHFd0vn gvarph@serv1";
   serv2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDE2TzrpOPaQ3Htd51zqES06PePo1E8fb9bemh9iQOJS gvarph@serv2";
+  desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOWiHIItEoZ3zsMzBUL11dj2iOyQ5tQapVRmqvdHf7lx gvarph@nixos";
 in {
-  "nas_auth.age".publicKeys = [serv1 serv2];
-  "homelab_k3s_token.age".publicKeys = [serv1 serv2];
+  "nas_auth.age".publicKeys = [serv1 serv2 desktop];
+  "homelab_k3s_token.age".publicKeys = [serv1 serv2 desktop];
 }
