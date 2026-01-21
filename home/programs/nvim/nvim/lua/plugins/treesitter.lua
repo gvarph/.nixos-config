57bda950d7c433fa1684655759000d9d86dc9d32
Filip Krul <gvarph006@gmail.com>
2026-01-21 17:43:39 +0100

    Fix treesitter

--- home/programs/nvim/nvim/lua/plugins/treesitter.lua
+++ home/programs/nvim/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,22 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPost", "BufNewFile" },
-	main = "nvim-treesitter.configs",
-	dev = true,
-	opts = {
-		autotag = {
-			enable = true,
-		},
-		highlight = {
-			-- `false` will disable the whole extension
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "gnn", -- set to `false` to disable one of the mappings
-				node_incremental = "grn",
-				scope_incremental = "grc",
-				node_decremental = "grm",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.config").setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
 			},
-		},
-	},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
+		})
+	end,
 }
