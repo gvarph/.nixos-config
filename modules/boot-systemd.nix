58efc603743f2c766a0c12e228b1eeed1cd7e9e0
Filip Krul <gvarph006@gmail.com>
2026-03-11 17:07:20 +0100

    Revert "downgrade_kernel"
    This reverts commit aee27541d68e9e11395339422039afb5047046d5.

--- modules/boot-systemd.nix
+++ modules/boot-systemd.nix
@@ -1,11 +1,10 @@
-# Standard systemd-boot configuration with stable LTS kernel
+# Standard systemd-boot configuration with latest kernel
 {pkgs, ...}: {
   boot = {
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    # Using 6.12 LTS for better hardware compatibility (especially DisplayLink)
-    kernelPackages = pkgs.linuxPackages_6_12;
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 }
