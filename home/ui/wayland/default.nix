c3587dfcb9e4a8a3914b9646ae947bd7b624bf9b
Filip Krul <gvarph006@gmail.com>
2026-03-11 17:06:48 +0100

    Hyprcursor

--- home/ui/wayland/default.nix
+++ home/ui/wayland/default.nix
@@ -7,6 +7,7 @@
   home.packages = with pkgs; [
     microsoft-edge
     mongodb-compass
+    hyprcursor
   ];
 
   imports = [
@@ -22,5 +23,14 @@
   home.sessionVariables = {
     ELECTRON_OZONE_PLATFORM_HINT = "wayland";
     ELECTRON_ENABLE_WAYLAND = "1";
+  };
+
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+    gtk.enable = true;
+    x11.enable = true;
+    hyprcursor.enable = true;
   };
 }
