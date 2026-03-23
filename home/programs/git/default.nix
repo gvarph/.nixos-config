22e06f04e3d3faf5dd9eb75a821109f8d28464e3
Filip Krul <gvarph006@gmail.com>
2026-03-23 10:09:53 +0100

    Clean up deprecated behaviour

--- home/programs/git/default.nix
+++ home/programs/git/default.nix
@@ -13,10 +13,10 @@
       enable = true;
     };
 
-    # signing = {
-    #   key = "~/.ssh/id_ed25519.pub";
-    #   signByDefault = true;
-    # };
+    signing = {
+      signByDefault = true;
+      format = null;
+    };
 
     settings = {
       user = {
