3d0efc25690221bbe500037edbd0ae9d80e5389b
Filip Krul <gvarph006@gmail.com>
2026-04-10 21:09:42 +0200

    cavemen

--- home/programs/opencode/default.nix
+++ home/programs/opencode/default.nix
@@ -1,8 +1,16 @@
 {
   config,
   pkgs,
+  lib,
   ...
-}: {
+}: let
+  caveman = pkgs.fetchFromGitHub {
+    owner = "JuliusBrussee";
+    repo = "caveman";
+    rev = "92f892f2b99744a4501f35bce9a2e384878e4877";
+    hash = "sha256-EAlKoqJuTMib+gcLscMtpS8Zzq/D/LmIRoG3g/XKThc=";
+  };
+in {
   programs.opencode = {
     enable = true;
     settings = {
@@ -12,6 +20,9 @@
           url = "https://mcp.context7.com/mcp";
         };
       };
+    };
+    skills = {
+      caveman = "${caveman}/skills/caveman";
     };
   };
 }
