9ad9fd6e69cbc274c16b369048d3dad757b2dd74
Filip Krul <gvarph006@gmail.com>
2026-04-12 01:35:27 +0200

    fix sanoid

--- devices/nas1/disko/sanoid.nix
+++ devices/nas1/disko/sanoid.nix
@@ -1,44 +1,28 @@
-{pkgs, ...}: let
-  # List only the datasets you want to snapshot and backup
-  monitoredDatasets = [
-    "flash"
-    "garage-s3"
-    "immich"
-    "music-assistant"
-    "nextcloud"
-    "obsidian"
-    "paperless"
-  ];
-in {
+{pkgs, ...}: {
   services.sanoid = {
     enable = true;
-    # We use 'builtins.listToAttrs' to turn our list into the format Sanoid expects
-    datasets = builtins.listToAttrs (map (name: {
-        name = "rpool/${name}";
-        value = {
-          autosnap = true;
-          autoprune = true;
-          hourly = 36;
-          daily = 30;
-          monthly = 12;
-        };
-      })
-      monitoredDatasets);
+    datasets = {
+      "rpool/flash" = {
+        autosnap = true;
+        autoprune = true;
+        hourly = 36;
+        daily = 30;
+        monthly = 12;
+      };
+    };
   };
 
   # --- SYNCOID CONFIG ---
   services.syncoid = {
     enable = true;
-    commands = builtins.listToAttrs (map (name: {
-        name = "backup-${name}";
-        value = {
-          source = "rpool/${name}";
-          target = "tank/snapshots/${name}";
-          recursive = false;
-          sendOptions = "P";
-          recvOptions = "u";
-        };
-      })
-      monitoredDatasets);
+    commands = {
+      "backup-flash" = {
+        source = "rpool/flash";
+        target = "tank/snapshots/flash";
+        recursive = true;
+        sendOptions = "P";
+        recvOptions = "u";
+      };
+    };
   };
 }
