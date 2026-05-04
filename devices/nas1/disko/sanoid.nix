3f59f91765d823e0f8fa203f6e6fbe13419ccd62
Filip Krul <gvarph006@gmail.com>
2026-05-04 14:49:05 +0200

    Backup tank

--- devices/nas1/disko/sanoid.nix
+++ devices/nas1/disko/sanoid.nix
@@ -9,6 +9,27 @@
         daily = 30;
         monthly = 12;
       };
+      "rpool/immich" = {
+        autosnap = true;
+        autoprune = true;
+        hourly = 36;
+        daily = 30;
+        monthly = 12;
+      };
+      "rpool/storage" = {
+        autosnap = true;
+        autoprune = true;
+        hourly = 36;
+        daily = 30;
+        monthly = 12;
+      };
+      "rpool/paperless" = {
+        autosnap = true;
+        autoprune = true;
+        hourly = 36;
+        daily = 30;
+        monthly = 12;
+      };
     };
   };
 
@@ -19,6 +40,27 @@
       "backup-flash" = {
         source = "rpool/flash";
         target = "tank/snapshots/flash";
+        recursive = true;
+        sendOptions = "P";
+        recvOptions = "u";
+      };
+      "backup-immich" = {
+        source = "rpool/immich";
+        target = "tank/snapshots/immich";
+        recursive = true;
+        sendOptions = "P";
+        recvOptions = "u";
+      };
+      "backup-storage" = {
+        source = "rpool/storage";
+        target = "tank/snapshots/storage";
+        recursive = true;
+        sendOptions = "P";
+        recvOptions = "u";
+      };
+      "backup-paperless" = {
+        source = "rpool/paperless";
+        target = "tank/snapshots/paperless";
         recursive = true;
         sendOptions = "P";
         recvOptions = "u";
