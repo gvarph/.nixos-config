f03aacc726d23590b6047c5f4eac3c4e17afbe43
Filip Krul <gvarph006@gmail.com>
2026-05-04 14:49:31 +0200

    Fix home assistant port forward

--- devices/nas1/nginx.nix
+++ devices/nas1/nginx.nix
@@ -46,10 +46,6 @@
           proxyPass = "http://10.0.30.117:8123";
           proxyWebsockets = true;
           extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
             add_header Strict-Transport-Security "max-age=63072000; preload" always;
           '';
         };
