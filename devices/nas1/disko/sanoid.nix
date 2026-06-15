edfc1358438912fe4fbbd99deb6dd9d068981b74
Filip Krul <gvarph006@gmail.com>
2026-06-16 00:34:34 +0200

    Cleanup sanoid

--- devices/nas1/disko/sanoid.nix
+++ devices/nas1/disko/sanoid.nix
@@ -9,21 +9,21 @@
         daily = 30;
         monthly = 12;
       };
-      "rpool/immich" = {
+      "tank/immich" = {
         autosnap = true;
         autoprune = true;
         hourly = 36;
         daily = 30;
         monthly = 12;
       };
-      "rpool/storage" = {
+      "tank/storage" = {
         autosnap = true;
         autoprune = true;
         hourly = 36;
         daily = 30;
         monthly = 12;
       };
-      "rpool/paperless" = {
+      "tank/paperless" = {
         autosnap = true;
         autoprune = true;
         hourly = 36;
@@ -40,27 +40,6 @@
       "backup-flash" = {
         source = "rpool/flash";
         target = "tank/snapshots/flash";
-        recursive = true;
-        sendOptions = "P";
-        recvOptions = "u";
-      };
-      "backup-immich" = {
-        source = "rpool/immich";
-        target = "tank/snapshots/immich";
-        recursive = true;
-        sendOptions = "P";
-        recvOptions = "u";
-      };
-      "backup-storage" = {
-        source = "rpool/storage";
-        target = "tank/snapshots/storage";
-        recursive = true;
-        sendOptions = "P";
-        recvOptions = "u";
-      };
-      "backup-paperless" = {
-        source = "rpool/paperless";
-        target = "tank/snapshots/paperless";
         recursive = true;
         sendOptions = "P";
         recvOptions = "u";
