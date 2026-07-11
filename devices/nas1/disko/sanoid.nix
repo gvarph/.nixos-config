f0cc7b2253b42e839c7a3e7ce77b37b6b735b867
Filip Krul <gvarph006@gmail.com>
2026-07-11 22:04:32 +0200

    Use loop for backup config

--- devices/nas1/disko/sanoid.nix
+++ devices/nas1/disko/sanoid.nix
@@ -1,47 +1,50 @@
-{pkgs, ...}: {
-  services.sanoid = {
-    enable = true;
-    datasets = {
-      "rpool/flash" = {
-        autosnap = true;
-        autoprune = true;
-        hourly = 36;
-        daily = 30;
-        monthly = 12;
-      };
-      "tank/immich" = {
-        autosnap = true;
-        autoprune = true;
-        hourly = 36;
-        daily = 30;
-        monthly = 12;
-      };
-      "tank/storage" = {
-        autosnap = true;
-        autoprune = true;
-        hourly = 36;
-        daily = 30;
-        monthly = 12;
-      };
-      "tank/paperless" = {
-        autosnap = true;
-        autoprune = true;
-        hourly = 36;
-        daily = 30;
-        monthly = 12;
-      };
-    };
+{pkgs, ...}: let
+  snapshotPolicy = {
+    autosnap = true;
+    autoprune = true;
+    hourly = 36;
+    daily = 30;
+    monthly = 12;
   };
 
-  # --- SYNCOID CONFIG ---
+  datasets = [
+    # NVMe app data
+    "rpool/flash"
+    "rpool/flash/arr"
+    "rpool/flash/garage-s3"
+    "rpool/flash/immich"
+    "rpool/flash/jellyfin"
+    "rpool/flash/monitoring"
+    "rpool/flash/music-assistant"
+    "rpool/flash/nextcloud"
+    "rpool/flash/obsidian"
+    "rpool/flash/paperless"
+
+    # HDD datasets
+    "tank/immich"
+    "tank/storage"
+    "tank/paperless"
+  ];
+in {
+  services.sanoid = {
+    enable = true;
+
+    datasets = builtins.listToAttrs (map (name: {
+        inherit name;
+        value = snapshotPolicy;
+      })
+      datasets);
+  };
+
   services.syncoid = {
     enable = true;
+
     commands = {
       "backup-flash" = {
         source = "rpool/flash";
         target = "tank/snapshots/flash";
         recursive = true;
-        sendOptions = "P";
+        sendOptions = "Pb";
         recvOptions = "u";
       };
     };
