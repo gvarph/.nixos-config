62a2fe054220003a299d561a82c78cedb08ef479
Filip Krul <gvarph006@gmail.com>
2026-09-06 20:47:39 +0200

    Zen config

--- home/ui/packages/zen.nix
+++ home/ui/packages/zen.nix
@@ -13,9 +13,43 @@
 
     # Catppuccin theme (catppuccin/zen-browser), symlinked into the profile's
     # chrome/catppuccin and loaded via userChrome/userContent imports.
-    profiles.default.presets.catppuccin = {
-      enable = true;
-      flavor = "Mocha"; # Frappe | Latte | Macchiato | Mocha
+    profiles.default = {
+      presets.catppuccin = {
+        enable = true;
+        flavor = "Mocha"; # Frappe | Latte | Macchiato | Mocha
+      };
+
+      userChrome = ''
+        profile-button {
+          display: none !important;
+        }
+      '';
+
+      search = {
+        default = "ddg";
+
+        engines = {
+          github = {
+            name = "GitHub Search";
+            urls = [
+              {
+                template = "https://github.com/search?q={searchTerms}";
+              }
+            ];
+            definedAliases = ["@gh"];
+          };
+
+          youtube = {
+            name = "YouTube Search";
+            urls = [
+              {
+                template = "https://www.youtube.com/results?search_query={searchTerms}";
+              }
+            ];
+            definedAliases = ["@yt"];
+          };
+        };
+      };
     };
 
     nativeMessagingHosts = [
